03ddbe371c5267c2a82ce076bb2ad2d12d604aea
laf <neil@lathwood.co.uk>
2015-11-29 20:09:28 +0000

    Config fetch updates

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
-import HttpClientConfig from 'aurelia-auth/app.httpClient.config';
+import {FetchConfig} from 'aurelia-auth';
 import AppRouterConfig from 'router-config';
 import * as toastr from "toastr";
 import * as fetch from 'fetch';
 
-@inject(Router, HttpClientConfig, AppRouterConfig)
+@inject(Router, FetchConfig, AppRouterConfig)
 
 export class App {
-  constructor(router, httpClientConfig, appRouterConfig) {
+  constructor(router, fetchConfig, appRouterConfig) {
 
     this.router = router;
-    this.httpClientConfig = httpClientConfig;
+    this.fetchConfig = fetchConfig;
     this.appRouterConfig = appRouterConfig;
   };
 
   activate() {
-    this.httpClientConfig.configure();
+    this.fetchConfig.configure();
     this.appRouterConfig.configure();
   };
 }
